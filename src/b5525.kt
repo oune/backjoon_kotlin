b9fe6cc048b67f04d650b3d61eb090faf6198deb
Junjae Lee <ejj612o@gmail.com>
2021-07-26 20:08:08 +0900

    Initial commit

---
+++ src/b5525.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val n = readLine()!!.toInt()
+    val m = readLine()!!.toInt()
+    val s = readLine()!!
+
+    print(count(n, m, s, 0, 0, 0))
+}
+
+tailrec fun count(n: Int, m: Int, s: String, acc: Int, count: Int, start: Int): Int = when {
+    start >= m - 2 -> acc
+    s[start] == 'I' && s[start + 1] == 'O' && s[start + 2] == 'I' -> {
+        if (count + 1 == n)
+            count(n, m, s, acc + 1, count, start + 2)
+        else
+            count(n, m, s, acc, count + 1, start + 2)
+    }
+    else -> count(n, m, s, acc, 0, start + 1)
+}
