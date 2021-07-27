1cc53d69a0e2fce0b8c03e556e344da90bab4b33
Junjae Lee <ejj612o@gmail.com>
2021-07-27 20:19:56 +0900

    18870 성공

--- src/b18870.kt
+++ src/b18870.kt
@@ -1,5 +1,7 @@
 import java.io.BufferedReader
+import java.io.BufferedWriter
 import java.io.InputStreamReader
+import java.io.OutputStreamWriter
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))){
     val n = readLine()!!.toInt()
@@ -8,14 +10,19 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))){
         it.toInt()
     }
 
-    val composedList = list.sorted().distinct().toIntArray()
+    val composedList = list.sorted().distinct()
 
     val map = HashMap<Int, Int>()
     composedList.forEachIndexed { index, i ->
         map[i] = index
     }
 
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
     list.forEach{
-        print("${map[it]} ")
+        bw.write("${map[it]} ")
     }
+    bw.flush()
+    bw.close()
 }
+
+
