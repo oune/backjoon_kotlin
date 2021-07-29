0bcacbfff5affddf2ecb67774488ae9126729775
Junjae Lee <ejj612o@gmail.com>
2021-07-29 15:28:49 +0900

    7662 라이브러리 사용으로 구현 했으나 실패

---
+++ src/b7662.kt
@@ -0,0 +1,46 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.StringBuilder
+import java.util.*
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val testCase = readLine()!!.toInt()
+    val out = StringBuilder()
+
+    repeat(testCase) {
+        val calCount = readLine()!!.toInt()
+        val minHeap = PriorityQueue<Int>()
+        val maxHeap = PriorityQueue<Int>(Collections.reverseOrder())
+
+        repeat(calCount) {
+            val line = readLine()!!.split(" ")
+            val operator = line[0]
+            val operand = line[1].toInt()
+
+            when (operator){
+                "I" -> {
+                    minHeap.add(operand)
+                    maxHeap.add(operand)
+                }
+                "D" -> when {
+                    minHeap.isEmpty() -> { }
+                    operand == 1 -> {
+                        val max = maxHeap.poll()
+                        minHeap.remove(max)
+                    }
+                    operand ==  -1 -> {
+                        val min = minHeap.poll()
+                        maxHeap.remove(min)
+                    }
+                }
+            }
+        }
+
+        if (maxHeap.isNotEmpty())
+            out.append("${maxHeap.poll()} ${minHeap.poll()}\n")
+        else
+            out.append("EMPTY\n")
+    }
+
+    print(out.toString())
+}
