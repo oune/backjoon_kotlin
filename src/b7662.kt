5485269944c279818033a057f7bb1957060dda7a
Junjae Lee <ejj612o@gmail.com>
2021-07-29 19:46:51 +0900

    7762 코드 단순화

--- src/b7662.kt
+++ src/b7662.kt
@@ -2,12 +2,11 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
-import java.lang.StringBuilder
 import java.util.*
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val testCase = readLine()!!.toInt()
-    val out = BufferedWriter(OutputStreamWriter(System.out))
+    val out = BufferedWriter(OutputStreamWriter(System.`out`))
 
     repeat(testCase) {
         val calCount = readLine()!!.toInt()
@@ -18,33 +17,23 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
             val operator = line[0]
             val operand = line[1].toInt()
 
-            when (operator){
-                "I" -> {
+            when {
+                operator == "I" -> {
                     map[operand] = map.getOrDefault(operand, 0) + 1
                 }
-                "D" -> when {
-                    map.isEmpty() -> { }
-
-                    operand == 1 -> {
-                        val max = map.lastKey()
-                        map[max] = map[max]!! - 1
-
-                        if (map[max] == 0) {
-                            map.remove(max)
-                        }
+                map.isNotEmpty() && operator == "D" -> {
+                    val num = when (operand) {
+                        1 -> map.lastKey()
+                        -1 -> map.firstKey()
+                        else -> null
                     }
-                    operand ==  -1 -> {
-                        val min = map.firstKey()
-                        map[min] = map[min]!! -1
 
-                        if (map[min] == 0) {
-                            map.remove(min)
-                        }
-                    }
+                    map[num!!] = map[num]!! - 1
+                    if (map[num] == 0)
+                        map.remove(num)
                 }
             }
         }
-
         if (map.isNotEmpty())
             out.append("${map.lastKey()} ${map.firstKey()}\n")
         else
