80bbb6d01860b5da94e51648ec3c1d53a0aacf6d
Junjae Lee <ejj612o@gmail.com>
2021-08-02 23:03:32 +0900

    2852  [코딩중].

--- src/b2852.kt
+++ src/b2852.kt
@@ -7,6 +7,7 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))){
 
     var score = 0
     var preTime = Pair(0, 0)
+    var preScore = 0;
     var team1 = Pair(0, 0)
     var team2 = Pair(0, 0)
 
@@ -20,10 +21,26 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))){
             score--
         }
 
-        if (score > 0) {
-
-        } else if (score < 0) {
-
+        if (preScore == 0) {
+            preTime = Pair(minute, second)
         }
+
+        if (score == 0) {
+            if (preScore > 0) {
+                team1 = Pair(team1.first + minute - preTime.first, team1.second + second - preTime.second)
+            } else {
+                team2 = Pair(team2.first + minute - preTime.first, team2.second + second - preTime.second)
+            }
+        }
+
+        preScore = score
     }
+    if (preScore > 0) {
+        team1 = Pair(team1.first + 48 - preTime.first, team1.second - preTime.second)
+    } else {
+        team2 = Pair(team2.first + 48 - preTime.first, team2.second - preTime.second)
+    }
+
+    println("${team1.first}:${team1.second}")
+    println("${team2.first}:${team2.second}")
 }
