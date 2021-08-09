b0c5909c42e8a6c885ed0c7f530eb0203dd4c3c8
Junjae Lee <ejj612o@gmail.com>
2021-08-09 23:03:06 +0900

    1004 [성공]

--- src/b11727.kt
+++ src/b11727.kt
@@ -1,16 +1,16 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
+//
+//fun main() = with (BufferedReader(InputStreamReader(System.`in`))){
+//    val n = readLine().toInt()
+//    print(tiling(n))
+//}
 
-fun main() = with (BufferedReader(InputStreamReader(System.`in`))){
-    val n = readLine().toInt()
-    print(tiling(n))
-}
-
-fun tiling (n : Int): Int = when {
-    n == 1 -> 1
-    n == 2 -> 3
-    n % 2 == 0 -> tiling()
-    else -> {
-
-    }
-}
+//fun tiling (n : Int): Int = when {
+//    n == 1 -> 1
+//    n == 2 -> 3
+//    n % 2 == 0 -> tiling()
+//    else -> {
+//
+//    }
+//}
