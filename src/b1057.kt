de9dfdca9708c2649e548b5508ba1e47502e6db4
Junjae Lee <ejj612o@gmail.com>
2021-08-11 19:42:33 +0900

    1057 [성공]

---
+++ src/b1057.kt
@@ -0,0 +1,21 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import kotlin.math.pow
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val (n, kim, lim) = readLine().split(" ").map{ it.toInt() }
+    val a = kim - 1
+    val b = lim - 1
+
+    var res = -1
+    for (it in 1..n) {
+        val num = 2.0.pow(it).toInt()
+
+        if (a / num == b / num) {
+            res = it
+            break
+        }
+    }
+
+    print(res)
+}
