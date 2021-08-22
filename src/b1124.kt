af67552009830242f32fafbee41677596296c089
Junjae Lee <ejj612o@gmail.com>
2021-08-22 16:32:50 +0900

    1124 내가 푼거 성공

--- src/b1124.kt
+++ src/b1124.kt
@@ -4,19 +4,40 @@ import java.io.InputStreamReader
 fun main() = with (BufferedReader(InputStreamReader(System.`in`))) {
     val (a, b) = readLine().split(" ").map { it.toInt() }
 
-    val memo = Array<Boolean>(b + 1) { true }
-    memo[0] = false
-    memo[1] = false
+    val prime = Array<Boolean>(b + 1) { true }
+    prime[0] = false
+    prime[1] = false
 
     (2 .. b).forEach {
-        if (memo[it]) {
+        if (prime[it]) {
             var i = 2
             var divide = it * i
-            while(divide < b) {
-                if (it % divide == 0)
-                    memo[it] = false
+            while(divide <= b) {
+                prime[divide] = false
                 divide = it * ++i
             }
         }
     }
+
+    var underPrimeCount = 0
+
+    (a .. b).forEach {
+        var now = it
+        var count = 0
+        var i = 2
+        while (i <= now) {
+            if (prime[i]) {
+                while (now % i == 0) {
+                    count++
+                    now /= i
+                }
+            }
+            i++
+        }
+        if (prime[count]) {
+            underPrimeCount++
+        }
+    }
+
+    print(underPrimeCount)
 }
