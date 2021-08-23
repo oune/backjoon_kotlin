2d38940995795acb6936b0cca195eeebb5049bab
Junjae Lee <ejj612o@gmail.com>
2021-08-23 16:21:49 +0900

    일단 배열로 작섣ㅇ

---
+++ src/b2751.kt
@@ -0,0 +1,50 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val count = readLine().toInt()
+    val arr = Array<Int>(count) { 0 }
+
+    repeat(count) {
+        val input = readLine().toInt()
+        arr[it] = input
+    }
+    merge(arr, 0, count - 1)
+
+    val out = BufferedWriter(OutputStreamWriter(System.out))
+    arr.forEach {
+        out.appendLine(it.toString())
+    }
+    out.flush()
+}
+fun merge(arr: Array<Int>, left:Int, right:Int) {
+    if (left == right){
+        return
+    }
+    val mid = (left + right) / 2
+    merge(arr, left, mid)
+    merge(arr, mid + 1, right)
+
+    var lIdx = left
+    var rIdx = mid + 1
+    val list = mutableListOf<Int>()
+
+    while(lIdx <= mid && rIdx <= right) {
+        if (arr[lIdx] <= arr[rIdx]){
+            list.add(arr[lIdx++])
+        } else {
+            list.add(arr[rIdx++])
+        }
+    }
+
+    while (lIdx <= mid)
+        list.add(arr[lIdx++])
+    while (rIdx <= right)
+        list.add(arr[rIdx++])
+
+    (left .. right).forEachIndexed { index, i ->
+        arr[i] = list[index]
+    }
+}
