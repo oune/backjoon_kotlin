0375e0793d723741215aaf82d9ce8dcc188530df
Junjae Lee <ejj612o@gmail.com>
2021-12-27 23:15:47 +0900

    [성공] 11727

--- src/b10000/b11727.kt
+++ src/b10000/b11727.kt
@@ -1,18 +1,14 @@
 package b10000
 
-import java.io.BufferedReader
-import java.io.InputStreamReader
-//
-//fun b1000.b3000to9999.b10000.main() = with (BufferedReader(InputStreamReader(System.`in`))){
-//    val n = readLine().toInt()
-//    print(tiling(n))
-//}
+fun main() = with(System.`in`.bufferedReader()) {
+    val num = readLine().toInt()
 
-//fun tiling (n : Int): Int = when {
-//    n == 1 -> 1
-//    n == 2 -> 3
-//    n % 2 == 0 -> tiling()
-//    else -> {
-//
-//    }
-//}
+    val dp = IntArray(num + 1) { 0 }
+    dp[1] = 1
+    dp[2] = 3
+
+    for (i in 3 .. num)
+        dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007
+
+    print(dp[num])
+}
