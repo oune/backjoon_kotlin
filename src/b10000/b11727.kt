356689c7c9f6a0696c179ec0b8e535946508f881
Junjae Lee <ejj612o@gmail.com>
2021-12-27 23:29:10 +0900

    [수정] 11727

--- src/b10000/b11727.kt
+++ src/b10000/b11727.kt
@@ -4,8 +4,10 @@ fun main() = with(System.`in`.bufferedReader()) {
     val num = readLine().toInt()
 
     val dp = IntArray(num + 1) { 0 }
-    dp[1] = 1
-    dp[2] = 3
+    if (num > 0)
+        dp[1] = 1
+    if (num > 1)
+        dp[2] = 3
 
     for (i in 3 .. num)
         dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007
