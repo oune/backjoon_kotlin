7a7d73c31753ba64a60695465dcd4a0c131f41b1
Junjae Lee <ejj612o@gmail.com>
2022-05-07 15:06:49 +0900

    임시답

---
+++ src/kakoTest2205/t2.kt
@@ -0,0 +1,55 @@
+package test.kakoTest2205
+
+import java.util.*
+
+fun main() {
+    println(solution(intArrayOf(3, 2, 7, 2), intArrayOf(4, 6, 5, 1)))
+}
+
+fun solution(queue1: IntArray, queue2: IntArray): Int {
+    val left = LinkedList<Int>()
+    var leftTotal = 0
+    queue1.forEach {
+        left.offer(it)
+        leftTotal += it
+    }
+
+    val right = LinkedList<Int>()
+    var rightTotal = 0
+    queue2.forEach {
+        right.offer(it)
+        rightTotal += it
+    }
+
+    var answer: Int = -1
+
+    var cnt = 0
+    var leftZero = false
+    var rightZero = false
+    while (!(leftZero && rightZero)) {
+        if (leftTotal == rightTotal) {
+            answer = cnt
+            break
+        }
+        else if (leftTotal > rightTotal) {
+            val num = left.poll()
+            leftTotal -= num
+            rightTotal += num
+            right.offer(num)
+        } else { // left < right
+            val num = right.poll()
+            leftTotal += num
+            rightTotal -= num
+            left.offer(num)
+        }
+
+        if (left.isEmpty())
+            leftZero = true
+        if (right.isEmpty())
+            rightZero = true
+
+        cnt++
+    }
+
+    return answer
+}
