83ffaa6999e356e1db7fc82a36cb40c4c3eaf9f4
Junjae Lee <ejj612o@gmail.com>
2022-05-12 23:24:11 +0900

    [성공] 1166

---
+++ src/b1000/b1166.kt
@@ -0,0 +1,22 @@
+package test.b1000
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val (n, l, w, h) = readLine().split(" ").map{ it.toInt() }
+
+    var low = 0.0
+    var high = l.coerceAtLeast(w.coerceAtLeast(h)).toDouble()
+    l.coerceAtLeast()
+
+    for (i in 0..10000) {
+        val mid = (low + high) / 2.0
+        val cnt = (l / mid).toLong() * (w / mid).toLong() * (h / mid).toLong()
+
+        if (cnt >= n) {
+            low = mid
+        } else {
+            high = mid
+        }
+    }
+
+    print(low)
+}
