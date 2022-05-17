2ff8f7a83d81316128ac4c1f9049787c29984ff5
Junjae Lee <ejj612o@gmail.com>
2022-05-17 21:43:05 +0900

    [성공] 2636

--- src/b2000/b2636.kt
+++ src/b2000/b2636.kt
@@ -13,60 +13,59 @@ fun main() = with(System.`in`.bufferedReader()) {
     while (mainQue.isNotEmpty()) {
         val que = LinkedList<Cheese>()
 
-        while(mainQue.isNotEmpty()) {
-            que.offer(mainQue.poll())
+        val time = mainQue.peek().time
+
+        if (mainQue.isNotEmpty()) {
+            que.offer(Cheese(0, 0, mainQue.pop().time))
+            mainQue.clear()
         }
+
 
         val visited = Array(arr.size) {
             BooleanArray(arr[it].size) { false }
         }
         while (que.isNotEmpty()) {
-            val (x, y, time) = que.poll()
+            val (x, y, _) = que.poll()
 
             if (visited[y][x]) {
                 continue
             }
+
             visited[y][x] = true
 
             if (arr[y][x] == 1) {
-                arr[y][x] = time + 1
-                mainQue.offer(Cheese(x, y, arr[y][x]))
-                continue
-            }
+                arr[y][x] = time - 1
+                mainQue.offer(Cheese(x, y, time - 1))
+            } else {
+                listOf (
+                    {x:Int, y:Int -> Pair(x + 1, y)},
+                    {x:Int, y:Int -> Pair(x - 1, y)},
+                    {x:Int, y:Int -> Pair(x, y + 1)},
+                    {x:Int, y:Int -> Pair(x, y - 1)}
+                ).forEach {
+                    val moved = it(x, y)
 
-            listOf (
-                {x:Int, y:Int -> Pair(x + 1, y)},
-                {x:Int, y:Int -> Pair(x - 1, y)},
-                {x:Int, y:Int -> Pair(x, y + 1)},
-                {x:Int, y:Int -> Pair(x, y - 1)}
-            ).forEach {
-                val moved = it(x, y)
-
-                if (visited.isInIndices(moved.first, moved.second) && !visited[moved.second][moved.first]) {
-                    que.offer(Cheese(moved.first, moved.second, arr[y][x]))
+                    if (visited.isInIndices(moved.first, moved.second) && !visited[moved.second][moved.first]) {
+                        que.offer(Cheese(moved.first, moved.second, arr[y][x]))
+                    }
                 }
             }
         }
-
-        arr.forEach {
-            println(it)
-        }
-        println()
     }
 
-    val max = arr.fold(0) { acc, list ->
-        acc.coerceAtLeast(list.fold(0) { innerAcc, i ->
-            innerAcc.coerceAtLeast(i)
+    val max = - arr.fold(0) { acc, list ->
+        acc.coerceAtMost(list.fold(0) { innerAcc, i ->
+            innerAcc.coerceAtMost(i)
         })
     }
     val cnt = arr.fold(0) { acc, list ->
         acc + list.count {
-            it == max
+            it == - max
         }
     }
 
     if (max != 0) {
-        println(max - 1)
+        println(max)
         println(cnt)
     } else {
         println(0)
