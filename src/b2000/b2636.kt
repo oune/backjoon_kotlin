55bea7b9a011be7ba2822ac6427ed0d1e9a5690c
Junjae Lee <ejj612o@gmail.com>
2022-05-17 21:14:58 +0900

    [중간저장] 2636 -1

--- src/b2000/b2636.kt
+++ src/b2000/b2636.kt
@@ -8,36 +8,70 @@ fun main() = with(System.`in`.bufferedReader()) {
         readLine().split(" ").map { it.toInt() }.toMutableList()
     }
 
-    val visited = Array(arr.size) {
-        BooleanArray(arr[it].size) { false }
-    }
+    val mainQue = LinkedList<Cheese>()
+    mainQue.offer(Cheese(0, 0, 0))
+    while (mainQue.isNotEmpty()) {
+        val que = LinkedList<Cheese>()
 
-    val que = LinkedList<Cheese>()
-    que.offer(Cheese(0, 0, 0))
-
-    while (que.isNotEmpty()) {
-        val (x, y, time) = que.poll()
-        visited[y][x] = true
-
-        if (arr[y][x] == time) {
-            arr[y][x] = time + 1
+        while(mainQue.isNotEmpty()) {
+            que.offer(mainQue.poll())
         }
 
-        listOf (
-            {x:Int, y:Int -> Pair(x + 1, y)},
-            {x:Int, y:Int -> Pair(x - 1, y)},
-            {x:Int, y:Int -> Pair(x, y + 1)},
-            {x:Int, y:Int -> Pair(x, y - 1)}
-        ).forEach {
-            val moved = it(x, y)
+        val visited = Array(arr.size) {
+            BooleanArray(arr[it].size) { false }
+        }
+        while (que.isNotEmpty()) {
+            val (x, y, time) = que.poll()
 
-            if (visited.isInIndices(moved.first, moved.second) && !visited[moved.second][moved.first]) {
-                que.offer(Cheese(moved.first, moved.second, arr[y][x]))
+            if (visited[y][x]) {
+                continue
+            }
+            visited[y][x] = true
+
+            if (arr[y][x] == 1) {
+                arr[y][x] = time + 1
+                mainQue.offer(Cheese(x, y, arr[y][x]))
+                continue
+            }
+
+            listOf (
+                {x:Int, y:Int -> Pair(x + 1, y)},
+                {x:Int, y:Int -> Pair(x - 1, y)},
+                {x:Int, y:Int -> Pair(x, y + 1)},
+                {x:Int, y:Int -> Pair(x, y - 1)}
+            ).forEach {
+                val moved = it(x, y)
+
+                if (visited.isInIndices(moved.first, moved.second) && !visited[moved.second][moved.first]) {
+                    que.offer(Cheese(moved.first, moved.second, arr[y][x]))
+                }
             }
         }
+
+        arr.forEach {
+            println(it)
+        }
+        println()
     }
 
-    println(arr.contentDeepToString())
+    val max = arr.fold(0) { acc, list ->
+        acc.coerceAtLeast(list.fold(0) { innerAcc, i ->
+            innerAcc.coerceAtLeast(i)
+        })
+    }
+    val cnt = arr.fold(0) { acc, list ->
+        acc + list.count {
+            it == max
+        }
+    }
+
+    if (max != 0) {
+        println(max - 1)
+        println(cnt)
+    } else {
+        println(0)
+        println(0)
+    }
 }
 
 fun Array<BooleanArray>.isInIndices(x:Int, y:Int): Boolean{
