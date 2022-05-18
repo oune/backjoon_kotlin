99d330ba534e48ffc673ca5c5196ba65f92b611f
Junjae Lee <ejj612o@gmail.com>
2022-05-18 21:54:36 +0900

    [성공] 5014

--- src/b5000/b5014.kt
+++ src/b5000/b5014.kt
@@ -6,35 +6,35 @@ fun main() = with(System.`in`.bufferedReader()) {
     val (f, s, g, u, d) = readLine().split(" ").map { it.toInt() }
 
     var ans = -1
-    val visited = BooleanArray(f + 1) { false }
-    val que = LinkedList<State>()
-    que.offer(State(s, 0))
+    val visited = IntArray(f + 1) { 0 }
+    val que = LinkedList<Int>()
+    que.offer(s)
+    visited[s] = 1
 
     while(que.isNotEmpty()) {
         val now = que.poll()
-        visited[now.floor] = true
 
-        if (now.floor == g) {
-            ans = now.count
+        if (now == g) {
+            ans = visited[now]
             break
         }
 
-        val up = now.floor + u
-        if (up in 1..visited.lastIndex && !visited[up]) {
-            que.offer(State(up, now.count + 1))
+        val up = now + u
+        if (up in 1..visited.lastIndex && visited[up] == 0) {
+            que.offer(up)
+            visited[up] = visited[now] + 1
         }
 
-        val down = now.floor - d
-        if (down in 1..visited.lastIndex && !visited[down]) {
-            que.offer(State(down, now.count + 1))
+        val down = now - d
+        if (down in 1..visited.lastIndex && visited[down] == 0) {
+            que.offer(down)
+            visited[down] = visited[now] + 1
         }
     }
 
     if (ans != -1) {
-        print(ans)
+        print(ans - 1)
     } else {
         print("use the stairs")
     }
 }
-
-private data class State(val floor:Int, val count:Int)
