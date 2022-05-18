251ed58d2c546ed97fd78ab65f636e410035932d
Junjae Lee <ejj612o@gmail.com>
2022-05-18 21:44:43 +0900

    [메모리 초과] 5014

--- src/b5000/b5014.kt
+++ src/b5000/b5014.kt
@@ -5,6 +5,36 @@ import java.util.*
 fun main() = with(System.`in`.bufferedReader()) {
     val (f, s, g, u, d) = readLine().split(" ").map { it.toInt() }
 
+    var ans = -1
     val visited = BooleanArray(f + 1) { false }
-    val que = LinkedList<Int>()
+    val que = LinkedList<State>()
+    que.offer(State(s, 0))
+
+    while(que.isNotEmpty()) {
+        val now = que.poll()
+        visited[now.floor] = true
+
+        if (now.floor == g) {
+            ans = now.count
+            break
+        }
+
+        val up = now.floor + u
+        if (up in 1..visited.lastIndex && !visited[up]) {
+            que.offer(State(up, now.count + 1))
+        }
+
+        val down = now.floor - d
+        if (down in 1..visited.lastIndex && !visited[down]) {
+            que.offer(State(down, now.count + 1))
+        }
+    }
+
+    if (ans != -1) {
+        print(ans)
+    } else {
+        print("use the stairs")
+    }
 }
+
+private data class State(val floor:Int, val count:Int)
