9122b317f97257ef65daee6b0a9ae668fdcb4e13
Junjae Lee <ejj612o@gmail.com>
2022-05-19 17:00:25 +0900

    [리팩토링] 6593

--- src/b6000/b6593.kt
+++ src/b6000/b6593.kt
@@ -6,8 +6,8 @@ fun main() = with(System.`in`.bufferedReader()) {
     val sout = System.out.bufferedWriter()
 
     var isContinue = true
-    while(isContinue) {
-        val (l, r, c) = readLine().split(" ").map{ it.toInt() }
+    while (isContinue) {
+        val (l, r, c) = readLine().split(" ").map { it.toInt() }
 
         if (l == 0 && r == 0 && c == 0) {
             isContinue = false
@@ -41,7 +41,7 @@ fun main() = with(System.`in`.bufferedReader()) {
             }
 
             var ans = -1
-            while(que.isNotEmpty()) {
+            while (que.isNotEmpty()) {
                 val now = que.poll()
 
                 if (building[now.z][now.y][now.x] == 'E') {
@@ -50,14 +50,32 @@ fun main() = with(System.`in`.bufferedReader()) {
                 }
 
                 listOf(
-                    {x:Int, y:Int, z:Int, time:Int -> State(x + 1, y, z, time + 1)},
-                    {x:Int, y:Int, z:Int, time:Int -> State(x - 1, y, z, time + 1)},
-                    {x:Int, y:Int, z:Int, time:Int -> State(x, y + 1, z, time + 1)},
-                    {x:Int, y:Int, z:Int, time:Int -> State(x, y - 1, z, time + 1)},
-                    {x:Int, y:Int, z:Int, time:Int -> State(x, y, z + 1, time + 1)},
-                    {x:Int, y:Int, z:Int, time:Int -> State(x, y, z - 1, time + 1)}
+                    { state: State ->
+                        val (x, y, z, time) = state
+                        State(x + 1, y, z, time + 1)
+                    },
+                    { state: State ->
+                        val (x, y, z, time) = state
+                        State(x - 1, y, z, time + 1)
+                    },
+                    { state: State ->
+                        val (x, y, z, time) = state
+                        State(x, y + 1, z, time + 1)
+                    },
+                    { state: State ->
+                        val (x, y, z, time) = state
+                        State(x, y - 1, z, time + 1)
+                    },
+                    { state: State ->
+                        val (x, y, z, time) = state
+                        State(x, y, z + 1, time + 1)
+                    },
+                    { state: State ->
+                        val (x, y, z, time) = state
+                        State(x, y, z - 1, time + 1)
+                    }
                 ).forEach {
-                    val moved = it(now.x, now.y, now.z, now.time)
+                    val moved = it(now)
 
                     if (building.isInBuilding(moved)) {
                         if (building[moved.z][moved.y][moved.x] != '#') {
@@ -82,7 +100,7 @@ fun main() = with(System.`in`.bufferedReader()) {
     sout.close()
 }
 
-private fun Array<Array<CharArray>>.isInBuilding(state: State) :Boolean{
+private fun Array<Array<CharArray>>.isInBuilding(state: State): Boolean {
     if (state.z in this.indices) {
         if (state.y in this[state.z].indices) {
             if (state.x in this[state.z][state.y].indices) {
@@ -93,4 +111,4 @@ private fun Array<Array<CharArray>>.isInBuilding(state: State) :Boolean{
     return false
 }
 
-private data class State(val x:Int, val y:Int, val z:Int, val time:Int)
+private data class State(val x: Int, val y: Int, val z: Int, val time: Int)
