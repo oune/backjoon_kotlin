081699209231c3e993c3c0b6c75cb778b0df2a74
Junjae Lee <ejj612o@gmail.com>
2022-08-21 14:55:20 +0900

    아기상어 성공

--- src/b10000/b16236.kt
+++ src/b10000/b16236.kt
@@ -3,96 +3,76 @@ package test.b10000
 import java.util.*
 
 fun main() = with(System.`in`.bufferedReader()) {
-    data class Fish(val size:Int, val x: Int, val y: Int)
-
     val mapSize = readLine().toInt()
-    val fishes = LinkedList<Fish>()
     val space = Array(mapSize) {
         readLine().split(" ").map { it.toInt() }.toIntArray()
     }
-
     var baby = Shark(2, 0, 0)
-    space.forEachIndexed {
-            idx, list ->
-        list.forEachIndexed {
-                listIdx, num ->
+    space.forEachIndexed { idx, list ->
+        list.forEachIndexed { listIdx, num ->
             if (num == 9) {
                 baby = Shark(2, listIdx, idx)
                 space[idx][listIdx] = 0
-            } else if (num > 0) {
-                fishes.offer(Fish(num, listIdx, idx))
             }
         }
     }
+    data class State(val point: Point, val time:Int)
+
+    val que = PriorityQueue<State>(
+        compareBy<State>{
+            it.time
+        }.thenBy {
+            it.point.y
+        }.thenBy {
+            it.point.x
+        })
+    que.offer(State(Point(baby.x, baby.y), 0))
+
+    var visited = Array(space.size) {
+        BooleanArray(space[it].size) { false }
+    }
+    visited[baby.y][baby.x] = true
 
     var time = 0
-    while(fishes.isNotEmpty()) {
-        val eatable = fishes.filter {
-            it.size < baby.size
-        }
-
-        if (eatable.isEmpty()) {
-            break
-        }
-
-        val res = eatable.map {
-            val (_, x, y) = it // 도착점과 시작점 변경 필요
-            val start = Coordinate(baby.x, baby.y)
-            val destination = Coordinate(x, y)
-            Pair(it, getDistance(start, destination, baby, space))
-        }.filter{ it.second.time != -1 }.sortedBy { it.second.time }
-
-        if (res.isEmpty())
-            break
-
-        val shortest = res.first()
-        baby.x = shortest.second.x
-        baby.y = shortest.second.y
-        baby.eat()
-        time += shortest.second.time
-
-        fishes.remove(shortest.first)
-    }
-
-    print(time)
-}
-
-fun getDistance(from:Coordinate, to:Coordinate, baby:Shark, space:Array<IntArray>) :PositionState{
-    val visited = Array(space.size) { BooleanArray(space[it].size) { false } }
-    val que = LinkedList<PositionState>()
-    que.offer(PositionState(from.x, from.y, 0))
-    visited[from.y][from.x] = true
-
     while(que.isNotEmpty()) {
         val now = que.poll()
+        val size = space[now.point.y][now.point.x]
+        if (size != 0 && size < baby.size) {
+            time += now.time
+            space[now.point.y][now.point.x] = 0
+            baby.eat()
+            que.clear()
+            que.offer(State(now.point, 0))
+            visited = Array(space.size) { BooleanArray(space[it].size) { false } }
+            visited[now.point.y][now.point.x] = true
+            continue
+        }
 
-        listOf(
-            { x:Int, y:Int -> Pair(x, y - 1) },
-            { x:Int, y:Int -> Pair(x - 1, y) },
-            { x:Int, y:Int -> Pair(x + 1, y) },
-            { x:Int, y:Int -> Pair(x, y + 1) },
-        ).forEach {
-            val moved = it(now.x, now.y)
+        run {
+            listOf(
+                {from:Point -> Point(from.x,from.y - 1)},
+                {from:Point -> Point(from.x - 1, from.y)},
+                {from:Point -> Point(from.x + 1, from.y)},
+                {from:Point -> Point(from.x, from.y + 1)},
+            ).forEach {
+                val moved = it(now.point)
 
-            if (moved.first in visited.indices && moved.second in visited[moved.first].indices) {
-                if (!visited[moved.second][moved.first])
-                    if (space[moved.second][moved.first] <= baby.size)
-                        if (moved.first == to.x && moved.second == to.y) {
-                            return PositionState(to.x, to.y, now.time + 1)
-                        } else {
-                            que.offer(PositionState(moved.first, moved.second, now.time + 1))
-                            visited[now.y][now.x] = true
+                if (moved.y in space.indices && moved.x in space[moved.y].indices) {// idx 확인
+                    if (!visited[moved.y][moved.x]) {// 방문확인
+                        val fishSize = space[moved.y][moved.x]
+                        if (fishSize <= baby.size) {// 이동
+                            que.offer(State(moved, now.time + 1))
+                            visited[moved.y][moved.x] = true
                         }
+                    }
+                }
             }
         }
     }
 
-    return PositionState(baby.x, baby.y, -1)
+    println(time)
 }
-
-data class PositionState(val x:Int, val y:Int, val time:Int)
-
-data class Coordinate(val x:Int, val y:Int)
+data class Point(val x:Int, val y:Int)
 
 class Shark(var size: Int, var x:Int, var y:Int) {
     private var count = 0
