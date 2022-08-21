8bde54f29ded1b5809bc6801c314726ef93aaba8
Junjae Lee <ejj612o@gmail.com>
2022-08-21 11:59:46 +0900

    아기상어 일부 정답

--- src/b10000/b12851.kt
+++ src/b10000/b12851.kt
@@ -1,56 +1,56 @@
-package test.b10000
-
-import java.util.*
-
-fun main() = with(System.`in`.bufferedReader()) {
-    val (n, sister) = readLine().split(" ").map { it.toInt() }
-    val size = 100000 + 1
-    val visitedTime = IntArray(size) { -1 }
-
-    var min = Int.MAX_VALUE
-    var count = 0
-
-    val que = LinkedList<PositionState>()
-    que.offer(PositionState(n, 0))
-    visitedTime[n] = 0
-
-    while(que.isNotEmpty()) {
-        val now = que.poll()
-
-        if (now.subin == sister) {
-            min = now.time
-            count++
-            continue
-        }
-
-        listOf(
-            {state: PositionState ->
-                val (subin, time) = state
-                PositionState(subin + 1, time + 1)
-            },
-            {state: PositionState ->
-                val (subin, time) = state
-                PositionState(subin - 1, time + 1)
-            },
-            {state: PositionState ->
-                val (subin, time) = state
-                PositionState(subin * 2, time + 1)
-            },
-        ).forEach {
-            val moved = it(now)
-
-            if (moved.subin in visitedTime.indices) {
-                if (visitedTime[moved.subin] == -1 || visitedTime[moved.subin] == moved.time) {
-                    que.offer(moved)
-                    visitedTime[moved.subin] = moved.time
-                }
-            }
-        }
-    }
-
-
-    println(min)
-    println(count)
-}
-
-private data class State(val subin:Int, val time:Int)
+//package test.b10000
+//
+//import java.util.*
+//
+//fun main() = with(System.`in`.bufferedReader()) {
+//    val (n, sister) = readLine().split(" ").map { it.toInt() }
+//    val size = 100000 + 1
+//    val visitedTime = IntArray(size) { -1 }
+//
+//    var min = Int.MAX_VALUE
+//    var count = 0
+//
+//    val que = LinkedList<PositionState>()
+//    que.offer(PositionState(n, 0))
+//    visitedTime[n] = 0
+//
+//    while(que.isNotEmpty()) {
+//        val now = que.poll()
+//
+//        if (now.subin == sister) {
+//            min = now.time
+//            count++
+//            continue
+//        }
+//
+//        listOf(
+//            {state: PositionState ->
+//                val (subin, time) = state
+//                PositionState(subin + 1, time + 1)
+//            },
+//            {state: PositionState ->
+//                val (subin, time) = state
+//                PositionState(subin - 1, time + 1)
+//            },
+//            {state: PositionState ->
+//                val (subin, time) = state
+//                PositionState(subin * 2, time + 1)
+//            },
+//        ).forEach {
+//            val moved = it(now)
+//
+//            if (moved.subin in visitedTime.indices) {
+//                if (visitedTime[moved.subin] == -1 || visitedTime[moved.subin] == moved.time) {
+//                    que.offer(moved)
+//                    visitedTime[moved.subin] = moved.time
+//                }
+//            }
+//        }
+//    }
+//
+//
+//    println(min)
+//    println(count)
+//}
+//
+//private data class State(val subin:Int, val time:Int)
--- src/b10000/b16236.kt
+++ src/b10000/b16236.kt
@@ -36,30 +36,37 @@ fun main() = with(System.`in`.bufferedReader()) {
 
         val res = eatable.map {
             val (_, x, y) = it // 도착점과 시작점 변경 필요
-            getDistance(x, y, baby, space)
-        }.filter{ it.time != -1 }.sortedBy { it.time }
+            val start = Coordinate(baby.x, baby.y)
+            val destination = Coordinate(x, y)
+            Pair(it, getDistance(start, destination, baby, space))
+        }.filter{ it.second.time != -1 }.sortedBy { it.second.time }
 
         if (res.isEmpty())
             break
 
         val shortest = res.first()
-        time += shortest.time
+        baby.x = shortest.second.x
+        baby.y = shortest.second.y
+        baby.eat()
+        time += shortest.second.time
+
+        fishes.remove(shortest.first)
     }
 
     print(time)
 }
 
-fun getDistance(x:Int, y:Int, baby:Shark, space:Array<IntArray>) :PositionState{
+fun getDistance(from:Coordinate, to:Coordinate, baby:Shark, space:Array<IntArray>) :PositionState{
     val visited = Array(space.size) { BooleanArray(space[it].size) { false } }
     val que = LinkedList<PositionState>()
-    que.offer(PositionState(baby.x, baby.y, 0))
+    que.offer(PositionState(from.x, from.y, 0))
 
     while(que.isNotEmpty()) {
         val now = que.poll()
         visited[now.y][now.x] = true
 
         listOf(
-            { x:Int, y:Int -> Pair(x, y) },
+            { x:Int, y:Int -> Pair(x + 1, y) },
             { x:Int, y:Int -> Pair(x - 1, y) },
             { x:Int, y:Int -> Pair(x, y + 1) },
             { x:Int, y:Int -> Pair(x, y - 1) }
@@ -69,21 +76,22 @@ fun getDistance(x:Int, y:Int, baby:Shark, space:Array<IntArray>) :PositionState{
             if (moved.first in visited.indices && moved.second in visited[moved.first].indices) {
                 if (!visited[moved.second][moved.first])
                     if (space[moved.second][moved.first] <= baby.size)
-                        if (moved.first == x && moved.second == y) {
-                            baby.eat()
-                            return PositionState(x, y, now.time)
+                        if (moved.first == to.x && moved.second == to.y) {
+                            return PositionState(to.x, to.y, now.time + 1)
                         } else
                             que.offer(PositionState(moved.first, moved.second, now.time + 1))
             }
         }
     }
 
-    return -1
+    return PositionState(baby.x, baby.y, -1)
 }
 
 data class PositionState(val x:Int, val y:Int, val time:Int)
 
-class Shark(var size: Int, val x:Int, val y:Int) {
+data class Coordinate(val x:Int, val y:Int)
+
+class Shark(var size: Int, var x:Int, var y:Int) {
     private var count = 0
 
     fun eat() {
