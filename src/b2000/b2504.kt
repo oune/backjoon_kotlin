527dafc0dcf46ca7358e5a3cad22d5b921c0fcfd
Junjae Lee <ejj612o@gmail.com>
2022-08-22 20:41:17 +0900

    함수 위치 수정

--- src/b2000/b2504.kt
+++ src/b2000/b2504.kt
@@ -1,62 +1,62 @@
 package test.b2000
 
+private data class Node(val bracket:Char, val num:Int, val isNumber:Boolean)
+
 fun main() = with(System.`in`.bufferedReader()) {
-    fun check(input:String) :Int{
-        data class Node(val bracket:Char, val num:Int, val isNumber:Boolean)
-        val stack = mutableListOf<Node>()
+    println(check(readLine()))
+}
+private fun check(input:String) :Int{
+    val stack = mutableListOf<Node>()
 
-        fun sum(stack:MutableList<Node>, target:Char, multiple:Int) :Int {
-            var sum = 0
-            do {
-                if (stack.isEmpty())
-                    return 0
-
-                val now = stack.removeLast()
-
-                if (now.isNumber) {
-                    sum += now.num
-                } else if (now.bracket == target){
-                    if(sum == 0)
-                        sum = 1
-                    sum *= multiple
-                } else {
-                    return 0
-                }
-            } while(now.isNumber)
-            return sum
-        }
-
-        input.forEach {
-            val node = when(it) {
-                '(' -> {
-                    Node('(', 2, false)
-                } ')' -> {
-                    Node('n', sum(stack, '(', 2), true)
-                } ']' -> {
-                    Node('n', sum(stack, '[', 3), true)
-                } '[' -> {
-                    Node('[', 3, false)
-                } else -> {
-                    Node('x', 0, false)
-                }
-            }
-            if (node.num == 0)
-                return 0
-
-            stack.add(node)
-        }
-
-        var sum = 0
-        while(stack.isNotEmpty()) {
-            val node = stack.removeLast()
-            if (node.isNumber)
-                sum += node.num
-            else {
-                return 0
+    input.forEach {
+        val node = when(it) {
+            '(' -> {
+                Node('(', 2, false)
+            } ')' -> {
+                Node('n', sum(stack, '(', 2), true)
+            } ']' -> {
+                Node('n', sum(stack, '[', 3), true)
+            } '[' -> {
+                Node('[', 3, false)
+            } else -> {
+                Node('x', 0, false)
             }
         }
-        return sum
+        if (node.num == 0)
+            return 0
+
+        stack.add(node)
     }
 
-    println(check(readLine()))
+    var sum = 0
+    while(stack.isNotEmpty()) {
+        val node = stack.removeLast()
+        if (node.isNumber)
+            sum += node.num
+        else {
+            return 0
+        }
+    }
+    return sum
+}
+
+private fun sum(stack:MutableList<Node>, target:Char, multiple:Int) :Int {
+    var sum = 0
+    do {
+        if (stack.isEmpty())
+            return 0
+
+        val now = stack.removeLast()
+
+        if (now.isNumber) {
+            sum += now.num
+        } else if (now.bracket == target){
+            if(sum == 0)
+                sum = 1
+            sum *= multiple
+        } else {
+            return 0
+        }
+    } while(now.isNumber)
+    return sum
 }
