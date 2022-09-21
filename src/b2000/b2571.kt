a0cc55a56fbbf40ecd481bfafc249129a3e935a6
Junjae Lee <ejj612o@gmail.com>
2022-09-21 20:41:35 +0900

    성공

--- src/b2000/b2571.kt
+++ src/b2000/b2571.kt
@@ -1,41 +1,16 @@
 package b2000
 
 fun main() = with(System.`in`.bufferedReader()) {
-    val vertical = mutableListOf<Int>()
-    val horizontal = mutableListOf<Int>()
+    val arrSize = 101
+    val arr = Array(arrSize) { IntArray(arrSize) { 0 } }
 
-    val blacks = Array(readLine().toInt()) {
+    Array(readLine().toInt()) {
         val (x, y) = readLine().split(' ').map { it.toInt() }
 
-        horizontal.add(x)
-        horizontal.add(x + 10)
-        vertical.add(y)
-        vertical.add(y + 10)
-        Pair(x, y)
-    }
-
-    val rows = vertical.toSet().sorted()
-    val columns = horizontal.toSet().sorted()
-    val arr = Array(rows.size - 1) {
-        IntArray(columns.size - 1) { 0 }
-    }
-
-    blacks.forEach { (x, y) ->
-        for (i in rows.indexOf(y) until rows.indexOf(y + 10)) {
-            for (j in columns.indexOf(x) until columns.indexOf(x + 10)) {
-                val lengthX = columns[j + 1] - columns[j]
-                val lengthY = rows[i + 1] - rows[i]
-                arr[i][j] = lengthX * lengthY
+        for (i in y until y + 10) {
+            for (j in x until x + 10) {
+                arr[i][j] = 1
             }
-        }
-    }
-
-    val sumArr = arr.map { line ->
-        var pre = 0
-        line.map {
-            val ans = if (it == 0) 0 else it + pre
-            pre = ans
-            ans
         }
     }
 
@@ -53,9 +28,8 @@ fun main() = with(System.`in`.bufferedReader()) {
                         break
                     }
                     // size check
-                    val size = arr.slice(i..newI).sumOf {
-                        it.slice(j..newJ).sum()
-                    }
+                    val size = (newI - i + 1) * (newJ - j + 1)
+
                     if (size > max)
                         max = size
                 }
