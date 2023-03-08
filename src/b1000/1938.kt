5171da05ef820df3347542b0695a5f018b67f9eb
june <ejj612@naver.com>
2023-03-08 09:27:39 +0900

    백준 1938 성공

--- src/b1000/1938.kt
+++ src/b1000/1938.kt
@@ -8,6 +8,11 @@ import java.util.LinkedList
 최대 가중치는 1,000,000,000 >> 10억 Int 범위 내
 틀린 이유) 지나간 경로의 길이가 아니라
 경로에 포함된 도로의 개수, 따라서 중복된 도로는 개수를 세지 않음.
+따라서 지나간 경로의 길이를 포함하는 방식으로 카운트를 하는 것이 아니라
+최대비용으로 지나가는 경로를 따라 가면서 개수를 세는 방식으로 구현이 가능
+
+역추적 말고 그냥 추적은 불가능 한가?
+정방향 추적시 전체의 최대가 되는 경우가 아닌 그리디한 최대만 확인하게 되므로 정방향 추적은 불가능하다.
  */
 fun main() = with(System.`in`.bufferedReader()) {
     val cityCnt = readLine().toInt()
@@ -51,9 +56,32 @@ fun main() = with(System.`in`.bufferedReader()) {
     }
     tss()
 
-    fun bfs() {
+    fun bfs(): Int {
+        val que = LinkedList<Int>()
+        que.offer(startCity)
 
+        val visited = BooleanArray(maxArr.size) { false }
+        visited[startCity] = true
+
+        var count = 0
+        while(que.isNotEmpty()) {
+            val now = que.poll()
+
+            for ((next, cost) in map[now]) {
+                if (maxArr[now] + cost == maxArr[next]) {
+                    count++
+
+                    if (!visited[next]) {
+                        que.offer(next)
+                        visited[next] = true
+                    }
+                }
+            }
+        }
+
+        return count
     }
 
     println(maxArr[endCity])
+    println(bfs())
 }
