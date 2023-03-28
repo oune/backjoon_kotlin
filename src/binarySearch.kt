62ba8c496b67db2263c3c0917e36952360e77922
이준재 <ejj612@naver.com>
2023-03-28 09:05:08 +0900

    작성자 테스트

--- src/binarySearch.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    val num = listOf(1, 4)
-
-    val target = 3
-    val lower = num.binarySearch(target).let { if (it < 0) -it - 2 else it }
-    val upper = num.binarySearch(target).let { if (it < 0) -it - 1 else it }
-
-    println("$lower $upper")
-}
